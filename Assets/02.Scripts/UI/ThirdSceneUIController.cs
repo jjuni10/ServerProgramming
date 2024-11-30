ede6a23bc3adb2d6eca085351d06d6f37224fd69
minji411 <ydm020411@gmail.com>
2024-12-01 02:42:12 +0900

    결과 씬 효과 구현
    부자연스러운건 어떻게 고치지..

--- Assets/02.Scripts/UI/ThirdSceneUIController.cs
+++ Assets/02.Scripts/UI/ThirdSceneUIController.cs
@@ -19,11 +19,18 @@ public class ThirdSceneUIController : MonoBehaviour
     private float rateValue;
     private float pointHeight;
 
+    private int redGun = -1;
+    private int redRun = -1;
+    private int blueGun = -1;
+    private int blueRun = -1;
+
+    
+    private bool co_Done = true;
+
     void Start()
     {
         winTeam = GameManager.Instance.WinTeam;
         //SetPointHeight();
-        SetWinTeamUI();
     }
 
     public void SetPointHeight()
@@ -34,18 +41,88 @@ public class ThirdSceneUIController : MonoBehaviour
         for (int i = 0; i < GameManager.Instance.PlayerCount; i++)
         {
             player[i] = GameManager.Instance.GetPlayer(i);
+            player[i]._playerComponents.rigidbody.isKinematic = true;
 
             pointHeight = player[i]._currentValue.point * rateValue;
-            //Debug.Log($"[PointTest] pointHeight: {pointHeight}");
+            if (player[i].Team == ETeam.Red){
+                if (player[i].Role == ERole.Gunner) redGun = i;
+                if (player[i].Role == ERole.Runner) redRun = i;
+            }
+            else{
+                if (player[i].Role == ERole.Gunner) blueGun = i;
+                if (player[i].Role == ERole.Runner) blueRun = i;
+            }
+            //StartCoroutine(UpdatePlayerHeight(i)); 
 
-            platform[i].transform.localScale += new Vector3(0, pointHeight, 0);
-            platform[i].transform.position += new Vector3(0, pointHeight/2, 0);
-            player[i].transform.position += new Vector3(0, pointHeight, 0);
-            
-            //Debug.Log($"[PointTest] GameManager.Instance.points[i]: {GameManager.Instance.points[i]}");
-            player[i]._currentValue.point = GameManager.Instance.points[i];
-            player[i].SetPlayerPoint();
         }
+        //SetWinTeamUI();
+        StartCoroutine(ViewPoints());
+    }
+
+    private IEnumerator ViewPoints()
+    {
+        //* Gunner -> Runner -> SetWinTeamUI()
+
+        if (redGun >= 0)
+            StartCoroutine(UpdatePlayerHeight(redGun)); 
+        yield return new WaitUntil(() => co_Done);  // co_Done이 true가 될때까지
+
+        if (blueGun >= 0)
+            StartCoroutine(UpdatePlayerHeight(blueGun)); 
+        yield return new WaitUntil(() => co_Done);  // co_Done이 true가 될때까지
+
+        if (redRun >= 0)
+            StartCoroutine(UpdatePlayerHeight(redRun)); 
+        yield return new WaitUntil(() => co_Done);  // co_Done이 true가 될때까지
+
+        if (blueRun >= 0)
+            StartCoroutine(UpdatePlayerHeight(blueRun)); 
+        yield return new WaitUntil(() => co_Done);  // co_Done이 true가 될때까지
+
+        SetWinTeamUI();
+
+        yield return null;
+    }
+
+    private IEnumerator UpdatePlayerHeight(int playerIndex)
+    {
+        co_Done = false;
+
+        pointHeight = player[playerIndex]._currentValue.point * rateValue;
+        Vector3 playerPos = player[playerIndex].transform.position;
+        Vector3 formSca = platform[playerIndex].transform.localScale;
+        Vector3 formPos = platform[playerIndex].transform.position;
+        Vector3 playerPos_ = playerPos + new Vector3(0, pointHeight, 0);
+        Vector3 formSca_ = formSca + new Vector3(0, pointHeight, 0);
+        Vector3 formPos_ = formPos + new Vector3(0, pointHeight/2, 0);
+
+        do
+        {
+            float lerpSpeed = 0.1f * Time.deltaTime;
+            
+            // player[playerIndex].transform.position = Vector3.Lerp(playerPos, playerPos_, lerpSpeed);
+            // platform[playerIndex].transform.localScale = Vector3.Lerp(formSca, formSca_, lerpSpeed);
+            // platform[playerIndex].transform.position = Vector3.Lerp(formPos, formPos_, lerpSpeed);
+
+            Vector3 pP = playerPos_ * lerpSpeed;
+            Vector3 fS = formSca_ * lerpSpeed;
+            Vector3 fP = formPos_ * lerpSpeed;
+            pP.x = 0; fS.x = 0; fP.x = 0;
+            pP.z = 0; fS.z = 0; fP.z = 0;
+            
+            player[playerIndex].transform.position += pP;
+            platform[playerIndex].transform.localScale += fS;
+            platform[playerIndex].transform.position += fP;
+
+            yield return null; 
+        }while (Vector3.Distance(player[playerIndex].transform.position, playerPos_) > 0.1f);
+        
+        player[playerIndex].transform.position = playerPos_;
+        platform[playerIndex].transform.localScale = formSca_;
+        platform[playerIndex].transform.position = formPos_;
+        player[playerIndex].SetPlayerPoint();
+
+        co_Done = true;
     }
 
     public void SetWinTeamUI()
